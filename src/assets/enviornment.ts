02520c7afc32bc87042eddc31a4a2bb12ba98cbc
Dhruv Jha <Dh123ruv#@>
2025-10-05 17:50:02 +0530

    added env

--- src/assets/enviornment.ts
+++ src/assets/enviornment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/',
-  databaseUrl: 'localhost:5432',
-  appName: 'My App Dev'
+  production: true,
+  apiUrl: 'https://quizbackend-4eid.onrender.com/api'
 };
